c2f0d2638988fc02aad61c8e585740c81f871f62
Mslonoff <matthew.slonoff@gmail.com>
2024-01-25 19:33:58 -0500

    get and get by id working

--- server/server.js
+++ server/server.js
@@ -9,9 +9,12 @@ const { PORT, DATABASE_URL } = process.env;
 // const client = new pg.Client({ // this is for a deployed server
 //   connectionString: DATABASE_URL,
 // });
+// await client.connect();
 
 const { Pool } = pg;
+const app = express();
 const expressPort = 8005;
+app.use(express.json());
 
 const client  = new Pool({ // this is for local host
   user: 'matthewslonoff',
@@ -21,15 +24,27 @@ const client  = new Pool({ // this is for local host
   port: 5432,
 });
 
-await client.connect();
-
-const app = express();
-
-app.use(express.json());
 // get all vehicles in vehicles table
-app.get("/api/vehicles", (req, res) => {
+app.get("/api/vehicles", (req, res) => { // good to go
   client.query("SELECT * FROM vehicles").then((result) => {
     res.send(result.rows);
+  });
+});
+
+// get vehicle by index
+app.get('/api/vehicles/:id', (req, res) => { // good to go
+  const { id } = req.params;
+  client.query('SELECT * FROM vehicles WHERE id=$1', [id])
+  .then((result) => {
+    if (result.rows.length > 0) {
+      res.send(result.rows)
+    } else {
+      res.status(404).send('Sorry cannot find vehicle');
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
   });
 });
 // ***************** routes that wil be needed ************
